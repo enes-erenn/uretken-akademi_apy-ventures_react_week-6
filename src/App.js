f2d1c40ae9b550ffe929c70a613d5d18456f8bcd
Enes Eren <88148105+thenesern@users.noreply.github.com>
2022-04-28 12:43:49 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+// Dependencies and Packages
+import axios from "axios";
+import { Component } from "react";
+// Styles
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { isFetching: false };
+
+  // Component "Mount" Edildiğinde "Fetch" işlemini başlatıyoruz.
+  componentDidMount() {
+    // İlk Önce "Fetch" işleminin başladığını belirtmek için ve "Loading", tetiklemek için state'i değiştiriyoruz.
+    this.setState({ isFetching: true });
+
+    // Burada "Fetch" işleminden önce bir "Timeout" veriyorum.
+    setTimeout(
+      function () {
+        this.setState({ isFetching: false });
+      }.bind(this),
+      1000
+    );
+
+    // "Fetch" İşlemini Gerçekleştirip "state" üzerine yazıyoruz.
+    axios("https://jsonplaceholder.typicode.com/users").then((data) => {
+      this.setState({ users: data });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        {/* Burada "state"imizdeki "isFetching" ancak  "falsy" bir değere sahipse "List"i göster diyoruz.*/}
+        {!this.state.isFetching && (
+          <ul
+            style={{
+              display: "flex",
+              alignItems: "flex-start",
+              justifyContent: "flex-start",
+              gap: "1rem",
+              flexDirection: "column",
+              backgroundColor: "#001219",
+              width: "100%",
+              height: "100vh",
+              margin: "0",
+              padding: "0",
+            }}
+          >
+            <h2 style={{ color: "#ffffff", padding: "2rem 1rem 0 1rem" }}>
+              User List
+            </h2>
+            {this.state.users?.data?.map((user) => (
+              <li
+                style={{
+                  display: "flex",
+                  gap: "1rem",
+                  backgroundColor: "lightGray",
+                  width: "100%",
+                  padding: "4px 0",
+                  margin: "0",
+                }}
+                key={user?.id}
+              >
+                <p style={{ fontWeight: "bold", paddingLeft: "  1rem" }}>
+                  {user.id})
+                </p>
+                <p style={{ fontWeight: "bold" }}>{user.username}:</p>
+
+                <p>{user.name}</p>
+              </li>
+            ))}
+          </ul>
+        )}
+        {/* Burada "state"imizdeki "isFetching" ancak  "truthy" bir değere sahipse "Loading"i göster diyoruz.*/}
+        {this.state.isFetching && (
+          <p
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#001219",
+              width: "100%",
+              height: "100vh",
+              margin: "0",
+              padding: "0",
+              color: "#FFFFFF",
+            }}
+          >
+            Loading...
+          </p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
